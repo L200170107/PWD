6e369ede94ffb2be1eac370b8ebfe2bc101e9b31
L200170107 <43411931+L200170107@users.noreply.github.com>
2019-10-01 16:49:42 +0700

    Add files via upload

---
+++ Modul 3/kendali.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>variable</title>
+</head>
+<body>
+    <h1>Nilai</h1>
+    <form action="kendali.php" method="post">
+        <p>Masukkan Nilai Angka (0-100) : <input type="text" name='nilai' size='3'></p>
+        <p><input type="submit" value="Proses" name='submit'></p>
+    </form>
+    <?php
+    error_reporting(E_ALL^E_NOTICE);
+    $nilai = $_POST['nilai'];
+    $submit = $_POST['submit'];
+    if($submit){
+        if($nilai==''){
+            $huruf='Nilai kosong/belum diisi';
+        }
+        elseif($nilai<=20){
+            $huruf='E';
+        }
+        elseif($nilai<=40){
+            $huruf='D';
+        }
+        elseif($nilai<=60){
+            $huruf='C';
+        }
+        elseif($nilai<=80){
+            $huruf='B';
+        }
+        elseif($nilai<=100){
+            $huruf='A';
+        }
+        else{
+            $huruf='Nilai yang dimasukkan salah!';
+        }
+        echo "Nalai angka adalah $nilai</br>";
+        echo "Maka nilai huruf adalah $huruf";
+    }
+    ?>
+</body>
+</html>
